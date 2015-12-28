f72906e6389c6c0d05fd463b23ee280251b8d05a
sNAtttu <nsanteri@hotmail.com>
2015-12-29 00:31:06 +0200

    Added some functionality to SPA application

--- SportSchedSPA/main.js
+++ SportSchedSPA/main.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
     initWindow();
+    $('.pull-down').each(function () {
+        $(this).css('margin-top', $(this).parent().height() - $(this).height())
+    });
 });
 
 ko.bindingHandlers.datePicker = {
@@ -19,10 +22,45 @@ ko.bindingHandlers.datePicker = {
 };
 
 var vm = {
+    showMainPage: ko.observable(true),
+    EditedExercise: ko.observable(),
+    GetFormData: GetFormData,
+    EditExerciseDetails: EditExerciseDetails,
     title:ko.observable("Sport Scheduler"),
     exercises: ko.observableArray(),
+    filteredExercises: ko.observableArray(),
+    selectedPlannedExercise: ko.observable(),
     MyDate: ko.observable(new Date()),
+    ShowAll: ShowAll,
+    ExerciseDetailsClick: ExerciseDetailsClick,
+    CloseExerciseDetails: CloseExerciseDetails,
     log : ko.observable(""),
+};
+
+function GetFormData(formElement) {
+    console.log(formElement);
+    var serializedForm = $(formElement).serializeArray();
+    console.log(serializedForm);
+};
+
+function EditExerciseDetails(data) {
+    console.log(data);
+};
+
+function CloseExerciseDetails() {
+    console.log("back to calendar");
+    vm.showMainPage(true);
+};
+
+function ExerciseDetailsClick(exercise) {
+    vm.selectedPlannedExercise(exercise);
+    console.log(exercise);
+    vm.showMainPage(false);
+};
+
+function ShowAll() {
+    vm.filteredExercises([]);
+    vm.filteredExercises(vm.exercises());
 };
 
 function initWindow() {
@@ -32,10 +70,17 @@ function initWindow() {
             exercise["selectedExercise"] = ko.observable();
         });
         ko.utils.arrayPushAll(vm.exercises, resultData);
+        ko.utils.arrayPushAll(vm.filteredExercises, resultData);
         ko.applyBindings(vm);
     });
 };
 vm.MyDate.subscribe(function (date) {
+    vm.filteredExercises([]);
     var parsedDate = moment(date).format("YYYY-MM-DD");
+    ko.utils.arrayForEach(vm.exercises(), function (exercise) {
+        if (moment(exercise.ScheduledTime).format("YYYY-MM-DD") === parsedDate) {
+            vm.filteredExercises.push(exercise);
+        }
+    });
     console.log(parsedDate);
 });
